789448733b06c674e8b1a5255deab7b501e4e87b
somu440xcd125 <114680994+somu440xcd125@users.noreply.github.com>
2025-04-25 11:16:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,27 +5,22 @@ import dotenv from 'dotenv';
 import Connection from './database/db.js';
 import Routes from './routes/route.js';
 
-
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT||8000;
+const PORT = process.env.PORT || 8000;
 
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
 Connection(username, password);
 
-
-
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: ['http://localhost:5173', 'https://toddler.onrender.com'], // Allow both dev and production origins
     credentials: true,
 }));
 app.use('/api/users', Routes);
 
 app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
-
-
